f863290f15475333578953b1f821a4215037feec
Leonardo Tonetto <leotonetto@gmail.com>
2016-06-07 11:16:24 +0200

    v0.1 of wigledownloader
    There are still some important TODOs, but at least for now this
    should do the basic. Still requires more testing, but we'll do it
    as we go :-) #yolo

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # wigledownloader
+
 Downloads all APs from wigle.net from a given area
+
+## In a nutshell
+
+```bash
+pip install wigle
+git clone https://github.com/tonetto/wigledownloader.git
+mkdir ./files
+python2 wigledownloader/wigledownloader.py -u <user> -p <password> -o ./files/
+```
+
+Please read the rest of this document to understand how to specify which area you want to download APs from.
+
+## About it
+
+This is a (very) simple python code that downloads all available APs listed at wigle.net for a given area.
+
+## How to specify your desired area
+
+I was super lazy while coding this, so the only way for this (at the moment) is editing the source code.
+
+Inside the `wigledownloader.py` you'll find the following lines:
+
+```python
+        ...
+        self.latmin = 47.95
+        self.latmax = 48.43
+        self.lonmin = 11.00
+        self.lonmax = 12.15
+        self.latdiv = 6
+        self.londiv = 10
+        ## For the lazy: use this one
+        ## Do not modify this lazy map after this point since rows will be the same object...
+        #self.div_map = [[1]*self.londiv]*self.latdiv
+        ## Or you can do it like that
+        self.div_map = [[ 2, 2, 2, 2, 2, 2, 8, 2, 2, 2],
+                        [ 2, 2, 2, 2, 4, 3, 2, 5, 2, 2],
+                        [ 2, 4, 5, 4, 4, 5, 2, 4, 2, 2],
+                        [ 2, 4, 4, 8,18, 8, 8, 6, 2, 2],
+                        [ 2, 2, 3, 4,16, 8, 4, 2, 2, 2],
+                        [ 2, 2, 4, 4, 4, 4, 2, 2, 2, 2]]
+        ...
+```
+Please edit:
+* `latmin, latmax, lonmin, lonmax` which should be self-explanatory,
+* `latdiv` and `londiv` are the number of divisions you want along the latitude axis (y-axis) and along the longitude axis (x-axis), and finally
+* `div_map` with the number of subdivisions you would like inside each division.
+
+The following image represents the example that is hardcoded into this script:
+
+![Wigle Munich](https://github.com/tonetto/wigledownloader/blob/master/misc/wigle_munich.jpg "Wigle map of Munich")
+
+There are 6 divisions on the y-axis (latitude) and 10 divisions on the x-axis (longitude). The numbers inside each division represent the number of subdivisions within each division (it's not so complicated).
+
+If you do not want to bother picking these numbers manually, uncomment the lines (show in the code snippet above as `#self.div_map = [[1]*self.londiv]*self.latdiv`) and comment the long list with the same name below it.
+
+In either way, if the code finds more APs it can actually download from wigle (with a single query), the download of that subdivision is restarted. This time, the box is subdivided (again) into two smaller parts. This operation will repeat as long as needed. Therefore, even if you take the lazy approach it will eventually work.
+
+The problem of the lazy approach is that each time the number of downloaded APs reaches a limit, all the already downloaded are discarded before subdivided the area again.
+
+## Running it
+
+This should be very simple, but there are two ways of doing it:
+* From zero (you never ran it before for the same query):
+```bash
+python2 wigledownloader/wigledownloader.py -u <user> -p <password> -o ./files/
+```
+
+As soon as you run this code (and periodically) it writes in a file called `coord.remain` the remaining subdivisions it still has to download. You are going to use this file to restart your download if you ever have to stop it.
+
+* After running it for a while (restarting):
+```bash
+python2 wigledownloader/wigledownloader.py -u <user> -p <password> -o ./files/ --coordfile coord.remain
+```
+This way it should continue from where it left the last time it was running.
+
+## Output files
+
+For now it is simplying saving everything as a [pickle](https://docs.python.org/2/library/pickle.html "Python object serialization") file.
--- wigledownloader.py
+++ wigledownloader.py
@@ -29,7 +29,28 @@ except ImportError:
     print >> sys.stderr, 'Please install wigle (eg. pip install wigle)'
     sys.exit(1)
 
-import argparse, os, subprocess, shutil, glob, time
+import argparse, pickle, time
+
+def drange(start, stop, step):
+    """
+    Float point implementation of range()
+
+    Based on (but not exactly):
+    http://stackoverflow.com/questions/477486/python-decimal-range-step-value
+    """
+    ## few sanity checks first
+    if start < stop and step < 0:
+        raise RuntimeError('Wrong input variables, step should be > 0.')
+    if start > stop and step > 0:
+        raise RuntimeError('Wrong input variables, step should be < 0.')
+    r = start
+    while start < stop and r < stop:
+     	yield r
+     	r += step
+    while start > stop and r > stop:
+        yield r
+        r += step
+
 
 class WigleDownloader:
     """
@@ -43,39 +64,227 @@ class WigleDownloader:
              Ref.: [0][0] is the upper left box
              In case none is given, 1 is applied to all boxes
     """
-    def __init__( self, user, password, coordfile ):
-        self.wigle_user = user
-        self.wigle_password = password
+    ## Some constants
+    wigle_downloads_per_page = wigle.WIGLE_PAGESIZE
+    wigle_max_ap_per_query = 10000
+    ## These add up to 24h, time we would expect to have the quota renewed
+    wigle_timeout_backoff = [0.25*3600, ## 15 minutes
+                             0.25*3600,
+                             0.5*3600,
+                             1*3600,
+                             2*3600,
+                             4*3600,
+                             8*3600,
+                             8*3600]    ##  8 hours
+    file_default_remain = './coord.remain'
+    
+    def __init__( self, user, password, coordfile, outpath ):
+        try:
+            ## Wigle, wigle, wigle :-)
+            self.wigle = wigle.Wigle( user, password )
+        except wigle.WigleAuthenticationError as wae:
+            print >> sys.stderr, 'Authentication error for {1}.'.format(user)
+            print >> sys.stderr, wae.message
+            sys.exit(-1)
+        except wigle.WigleError as werr:
+            print >> sys.stderr, werr.message
+            sys.exit(-2)
+
+        self.outpath = outpath
+        self.coordfile = coordfile
+        ## This is for the city of Munich-DE
+        ## TODO: replace this with geocoding
         self.latmin = 47.95
         self.latmax = 48.43
         self.lonmin = 11.00
         self.lonmax = 12.15
         self.latdiv = 6
         self.londiv = 10
+        ## For the lazy: use this one
+        ## Do not modify this lazy map after this point since rows will be the same object...
+        #self.div_map = [[1]*self.londiv]*self.latdiv
+        ## Or you can do it like that
         self.div_map = [[ 2, 2, 2, 2, 2, 2, 8, 2, 2, 2],
                         [ 2, 2, 2, 2, 4, 3, 2, 5, 2, 2],
-                        [ 2, 4, 5, 4, 4, 4, 2, 4, 2, 2],
+                        [ 2, 4, 5, 4, 4, 5, 2, 4, 2, 2],
                         [ 2, 4, 4, 8,18, 8, 8, 6, 2, 2],
-                        [ 2, 2, 2, 4,16, 8, 4, 2, 2, 2],
+                        [ 2, 2, 3, 4,16, 8, 4, 2, 2, 2],
                         [ 2, 2, 4, 4, 4, 4, 2, 2, 2, 2]]
-        self.intervals = self._compute_intervals()
+        self.INTERVALS = []
+        self.REMAINING_INTERVALS = []
 
 
-    def _compute_intervals(self):
+    def run(self):
+        """
+        Just so that it does not look so ugly
+        """
+        ## We either call compute_intervals() or parse_coordfile()
+        if self.coordfile:
+            self.parse_coordfile()
+        else:
+            self.compute_intervals()
+
+        self.REMAINING_INTERVALS = self.INTERVALS[:]
+        self.REMAINING_INTERVALS.reverse()
+
+        ## Now we (continue) download(ing)
+        self.download()
+
+
+    def download(self):
+        """
+        Download whatever is inside self.INTERVALS using
+        wigle pythong API (not official apparently)
+        """
+        def callback_newpage(since):
+            pass
+        def _download( lat1, lat2, lon1, lon2, backoff_idx=0 ):
+            """
+            This one will be called recursively until the subdivision
+            is fully downloaded. In case it reaches 10k it breaks down
+            this subdivision into two parts by dividing the longitude
+            interval into two. Something like this:
+
+                         lat2
+                  -----------------
+                 |                 |              ^
+                 |                 |              | N
+            lon1 |                 | lon2
+                 |                 |
+                 |                 |
+                  -----------------
+                         lat1
+
+            Becomes:
+
+                         lat2
+                  -----------------
+                 |        |        |              ^
+                 |        |        |              | N
+            lon1 |        |        | lon2
+                 |        |lon1_5  |
+                 |        |        |
+                  -----------------
+                         lat1
+
+            """
+            print >> sys.stdout, 'Downloading ({0},{1},{2},{3})'.format( lat1, lat2, lon1, lon2 )
+            try:
+                RESULTS = self.wigle.search( lat_range   = ( lat1, lat2 ),
+                                             long_range  = ( lon1, lon2 ),
+                                             on_new_page = callback_newpage,
+                                             max_results = WigleDownloader.wigle_max_ap_per_query )
+                # Need to double check this
+                if len(RESULTS) >= 9998:
+                    print >> sys.stderr, 'Subdividing {0} {1} {2} {3}'.format(lat1,lat2,lon1,lon2)
+                    ## Will break down longitude interval into two parts
+                    lon1_5 = (lon2-lon1)/2.0
+                    R1 = _download( lat1, lat2, lon1, lon1_5 )
+                    R2 = _download( lat1, lat2, lon1_5, lon2 )
+                    RESULTS = R1.copy()
+                    RESULTS.update(R2)
+
+            except wigle.WigleRatelimitExceeded as wrle:
+                wait_s = WigleDownloader.wigle_timeout_backoff[backoff_idx]
+                print >> sys.stderr, 'Already got WigleRatelimitExceeded.'
+                print >> sys.stderr, 'Sleeping for {0} seconds before trying again.'.format(wait_s)
+                time.sleep(wait_s)
+                ## We may enter an infinite loop here...
+                ## TODO: solve it (for now check the stdout for problems)
+                return _download(lat1, lat2, lon1, lon2,
+                                 backoff_idx=(backoff_idx+1)%len(WigleDownloader.wigle_timeout_backoff))
+            except wigle.WigleError as we:
+                print >> sys.stderr, we
+                print >> sys.stderr, 'Something wrong with Wigle, stopping..'
+                raise
+            except KeyboardInterrupt:
+                print >> sys.stderr, 'Stopping the script.'
+                sys.exit(0)
+            except:
+                print >> sys.stderr, 'This looks like a bug.', sys.exc_info()[0]
+                return []
+            else:
+                sucess_string = 'Sucess downloading ({0},{1},{2},{3}) with {4} APs'
+                print >> sys.stdout, sucess_string.format( lat1, lat2, lon1, lon2, len(RESULTS) )
+                return RESULTS
+                
+        try:
+            ##
+            for interval in self.INTERVALS:
+                assert len(interval) == 4, 'Something wrong generating self.INTERVALS.'
+
+                lat1,lat2,lon1,lon2 = interval
+                AP_SUBDIVISION = _download( lat1, lat2, lon1, lon2 )
+
+                ## Write this out using pickle
+                ## TODO: write out as sqlite file
+                pickle_file = '{0}/{1}_{2}_{3}_{4}.p'.format( self.outpath, lat1, lat2, lon1, lon2 )
+                pickle.dump(AP_SUBDIVISION, open( pickle_file, "wb" ))
+                
+                ## Note: this was .reverse()'ed before
+                ## Pop'ing from the end of the list is much quicker
+                self.REMAINING_INTERVALS.pop()
+                
+                ## Write out coord.remain
+                with open( WigleDownloader.file_default_remain, 'wb' ) as coord_remain_file:
+                    for interval in self.REMAINING_INTERVALS:
+                        print >> coord_remain_file, ','.join(map(str,interval))
+
+        except KeyboardInterrupt:
+            print >> sys.stderr, 'Stopping the script.'
+            sys.exit(0)
+        except:
+            print >> sys.stderr, 'This looks like a bug.', sys.exc_info()[0]
+            sys.exit(-3)
+            
+
+    def compute_intervals(self):
         """
         Returns a list with tuples containing:
             [(box_lat_min,box_lat_max,box_lon_min,box_lon_max),...]
+        Since [0][0] is the upper left corner, lon grows positively
+        but lat grows negatively.
         """
         if len(self.div_map) != self.latdiv or len(self.div_map[0]) != self.londiv:
             raise RuntimeError('Map dimensions not correct!')
 
-        lat_setp = (self.latmax - self.latmin)
+        ## Compute the size of each initial box (in degrees).
+        lat_step = -(self.latmax - self.latmin) / self.latdiv
+        lon_step =  (self.lonmax - self.lonmin) / self.londiv
+
+        ## Compute the intervals.
+        initial_lat = self.latmax
+        initial_lon = self.lonmin
+        for row in self.div_map:
+            initial_lon = self.lonmin
+            for subdivisions in row:
+                lat_sub_step = lat_step / float(subdivisions)
+                lon_sub_step = lon_step / float(subdivisions)
+
+                ## min for each subdivision, for max we just add sub_step to it.
+                lats = list(drange(initial_lat,initial_lat+lat_step,lat_sub_step))
+                lons = list(drange(initial_lon,initial_lon+lon_step,lon_sub_step))
+
+                self.INTERVALS.extend([( lat, lat+lat_sub_step,
+                                         lon, lon+lon_sub_step ) for lat,lon in zip( lats, lons )])
+                initial_lon += lon_step
+            initial_lat += lat_step
+
 
     def parse_coordfile( self, coordfile ):
+        """
+        Parses the coord.remain file with the following format:
+
+        lat1,lat2,lon1,lon2
+
+        """
         with open(coordfile) as f:
             line = f.readline()
             while line:
-                line = line.strip()
+                COORDS = line.strip().split(',')
+                assert len(COORDS) == 4, 'Something is wrong with coord.remain file.'
+                self.INTERVALS.append(tuple(COORDS))
+        
 
 
 if __name__ == '__main__':
@@ -86,6 +295,10 @@ if __name__ == '__main__':
         '-p', '--password', help='Wigle password', required=True )
     parser.add_argument(
         '--coordfile', help='coord.remain file path', required=False, default=None )
+    parser.add_argument(
+        '-o', '--outpath', help='Path to store pickle files.')
 
     args = parser.parse_args()
-    wigledownloader = WigleDownloader(args.user, args.password, args.coordfile)
+    wigledownloader = WigleDownloader( args.user, args.password, args.coordfile, args.outpath )
+    ## Not recommended to put this in a subprocess.
+    wigledownloader.run()
